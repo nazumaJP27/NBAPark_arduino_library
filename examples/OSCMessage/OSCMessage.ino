c6646b49850429a6ccda9cc9f6ac9eb0f9caa988
NPG - J. Paulo Seibt <jose.neto@nbaparkgramado.com.br>
2025-02-24 17:12:51 -0300

    feat(OSCMessage): Add func to extract arguments from OSC messages
    Extracts the connected state from OSC messages sent by Resolume Arena
    when a specific clip starts playing. This allows for programmatic
    detection of clip changes and synchronization with other systems.

--- examples/OSCMessage/OSCMessage.ino
+++ examples/OSCMessage/OSCMessage.ino
@@ -2,7 +2,7 @@
 #include <OSCMessage.h>
 #include <Ethernet.h>
 #include <EthernetUDP.h>
-#include <stdio.h>
+#include <string.h> // strcmp
 
 BasketSensor basket(2, 3);
 uint8_t ball_count;
@@ -10,15 +10,16 @@ uint8_t ball_count;
 uint8_t board_mac[] = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0x04};
 IPAddress board_ip(172, 30, 6, 200);
 IPAddress pc_ip(172, 30, 6, 58);
-int resolume_port = 7000;
+int resolume_port = 7001;
 
 EthernetUDP udp;
 //IPAddress mvp4_pc_ip(172, 30, 4, 104) // NPG-MVP004 >NBAPARKDINAMICO/>172.30.4.104
 
-char osc_message_buffer[40];
+uint8_t osc_message_buffer[255];
 
 // Prototypes
 void send_osc_resolume(uint8_t in_ball_count);
+bool mvp_game_start(const uint8_t in_buffer);.
 
 
 void setup()
@@ -34,7 +35,16 @@ void setup()
 
 void loop()
 {
-    Serial.print("0\n");
+    int packet_size = udp.parsePacket();
+    if (packet_size)
+    {
+        udp.read(osc_message_buffer, 255);
+        if (mvp_game_start(osc_message_buffer))
+        {
+            Serial.println("GO!");
+        }
+    }
+
     if (basket.ball_detected())
     {
         Serial.print("Ball detected! count: ");
@@ -49,11 +59,36 @@ void loop()
 void send_osc_resolume(uint8_t in_ball_count)
 {
     // Construct the OSC address using snprintf to write into a buffer and inserting the ball count variable
-    snprintf(osc_message_buffer, sizeof(osc_message_buffer), "/composition/layers/2/clips/%d/connect", in_ball_count + 2);
+    snprintf((char*)osc_message_buffer, sizeof(osc_message_buffer), "/composition/layers/2/clips/%d/connect", in_ball_count + 2);
 
-    OSCMessage msg(osc_message_buffer);
+    OSCMessage msg((char*)osc_message_buffer);
 
     udp.beginPacket(pc_ip, resolume_port);
     msg.send(udp);
     udp.endPacket();
+}
+
+bool mvp_game_start(const uint8_t* in_buffer)
+{
+    const uint8_t* ptr = in_buffer;
+
+    // Extract and compare address (null-terminated, 4-byte aligned)
+    char address[64];
+    uint8_t addr_len = strnlen((const char*)ptr, sizeof(address));
+    memcpy(address, ptr, addr_len);
+    address[addr_len] = '\0';
+
+    ptr += (addr_len + 4) & ~3; // Move to 4-byte boundary
+    uint8_t type_len = strnlen((const char*)ptr, 1);
+    ptr += (type_len + 4) & ~3;
+
+    char res_clip_addr[] = "/composition/layers/1/clips/2/connected";
+    if (strcmp(address, res_clip_addr) == 0)
+    {
+        int32_t value;
+        memcpy(&value, ptr, sizeof(value));
+        value = __builtin_bswap32(value);  // Convert from big-endian
+        return value == 3;
+    }
+    return false;
 }
